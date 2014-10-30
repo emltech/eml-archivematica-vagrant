26fe680ee21bfa9365dd592de98dce1096b0054d
EML Tech <uofmmedialab@gmail.com>
2014-10-30 12:11:47 -0500

    initial commit

---
+++ Vagrantfile
@@ -0,0 +1,26 @@
+# -*- mode: ruby -*-
+# vi: set ft=ruby :
+
+# Vagrantfile API/syntax version. Don't touch unless you know what you're doing!
+VAGRANTFILE_API_VERSION = "2"
+
+Vagrant.configure(VAGRANTFILE_API_VERSION) do |config|
+  config.vm.box = "hashicorp/precise32"
+
+  config.vm.network "forwarded_port", guest: 80, host: 8080
+  config.vm.network "forwarded_port", guest: 8000, host: 8000
+  
+  #nearby folder on host for novice users to appropriate folder on guest for Archivematica use.
+  config.vm.synced_folder "archivematica_data", "/home/vagrant/archivematica_data"
+
+  #good values for testing, may need more with content
+  config.vm.provider "virtualbox" do |v|
+     v.name = "archivematica_tested_provisioner"
+     v.memory = 1024
+     v.cpus = 2
+     v.customize ["modifyvm", :id, "--cpuexecutioncap", "75"]
+  end
+  
+  #install everything needed for archivematica
+  config.vm.provision "shell", path: "archivematica_provisioner.sh"
+end
---
+++ archivematica_provisioner.sh
@@ -0,0 +1,77 @@
+declare -r MYSQL_PASS=YOUR_PASSWORD
+
+sudo apt-get update
+sudo apt-get install -y python-software-properties
+sudo add-apt-repository -y ppa:archivematica/release
+sudo add-apt-repository -y ppa:archivematica/externals
+
+sudo wget -O - http://packages.elasticsearch.org/GPG-KEY-elasticsearch | sudo apt-key add -
+
+cat << EOF | sudo tee -a /etc/apt/sources.list
+deb http://packages.elasticsearch.org/elasticsearch/0.90/debian stable main
+EOF
+
+#Workaround for unattended GRUB upgrade. On upgrade, GRUB will ask for user input 
+#for configuration which will upset the vagrant install process.
+unset UCF_FORCE_CONFFOLD
+export UCF_FORCE_CONFFNEW=YES
+ucf --purge /boot/grub/menu.lst
+
+export DEBIAN_FRONTEND=noninteractive
+apt-get update
+apt-get -o Dpkg::Options::="--force-confnew" --force-yes -fuy upgrade
+
+#set installation configs early to avoid use input for mysql, mcp-server, and postfix
+#ran debconf-get-selections after a successful install and got these options.
+sudo apt-get install -y debconf-utils
+cat << EOF | debconf-set-selections
+mysql-server-5.0 mysql-server/root_password password "$MYSQL_PASS"
+mysql-server-5.0 mysql-server/root_password_again password "$MYSQL_PASS"
+mysql-server-5.0 mysql-server/root_password seen true
+mysql-server-5.0 mysql-server/root_password_again seen true
+archivematica-mcp-server archivematica-mcp-server/password-confirm password "$MYSQL_PASS"
+archivematica-mcp-server archivematica-mcp-server/mysql/admin-pass password "$MYSQL_PASS"
+archivematica-mcp-server archivematica-mcp-server/app-password-confirm test
+archivematica-mcp-server archivematica-mcp-server/db/dbname string MCP
+archivematica-mcp-server archivematica-mcp-server/database-type select mysql
+archivematica-mcp-server archivematica-mcp-server/mysql/method select unix socket
+archivematica-mcp-server archivematica-mcp-server/internal/reconfiguring boolean false
+archivematica-mcp-server archivematica-mcp-server/install-error select abort
+archivematica-mcp-server archivematica-mcp-server/upgrade-error select abort
+archivematica-mcp-server archivematica-mcp-server/dbconfig-install boolean true
+archivematica-mcp-server archivematica-mcp-server/internal/skip-preseed boolean false
+archivematica-mcp-server archivematica-mcp-server/upgrade-backup boolean true
+archivematica-mcp-server archivematica-mcp-server/mysql/admin-user string root
+archivematica-mcp-server archivematica-mcp-server/db/app-user string archivematica
+archivematica-mcp-server archivematica-mcp-server/remove-error select abort
+archivematica-mcp-server archivematica-mcp-server/dbconfig-upgrade boolean true
+archivematica-mcp-server archivematica-mcp-server/missing-db-package-error select abort
+archivematica-mcp-server archivematica-mcp-server/dbconfig-reinstall boolean false
+postfix postfix/mailname string /etc/mailname
+postfix postfix/main_mailer_type select No configuration
+postfix postfix/rfc1035_violation boolean false
+postfix postfix/mynetworks string 127.0.0.0/8 [::ffff:127.0.0.0]/104 [::1]/128
+postfix postfix/mailbox_limit string 0
+EOF
+
+sudo apt-get install -y archivematica-storage-service
+sudo apt-get install -y archivematica-mcp-server
+sudo apt-get install -y archivematica-mcp-client
+sudo apt-get install -y archivematica-dashboard
+sudo apt-get install -y elasticsearch
+
+sudo wget -q https://raw.githubusercontent.com/artefactual/archivematica/stable/1.3.x/localDevSetup/apache/apache.default -O /etc/apache2/sites-available/default
+sudo rm /etc/nginx/sites-enabled/default
+sudo ln -s /etc/nginx/sites-available/storage /etc/nginx/sites-enabled/storage
+sudo ln -s /etc/uwsgi/apps-available/storage.ini /etc/uwsgi/apps-enabled/storage.ini
+
+sudo service uwsgi restart
+sudo service nginx restart
+sudo /etc/init.d/apache2 restart
+sudo freshclam
+sudo /etc/init.d/clamav-daemon start
+sudo /etc/init.d/elasticsearch restart
+sudo /etc/init.d/gearman-job-server restart
+sudo start archivematica-mcp-server
+sudo start archivematica-mcp-client
+sudo start fits
